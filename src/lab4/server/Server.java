cb2de7b2e7f0c03ae661e076983a2659bb415406
imcode <128029980+ilyas11m@users.noreply.github.com>
2025-02-28 17:21:39 +0300

    server some features realised

--- src/lab4/server/Server.java
+++ src/lab4/server/Server.java
@@ -3,15 +3,18 @@ package lab4.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Server {
+
+    public static void main(String[] args) {
+        new Server(args);
+    }
+
     public static int[][] arr1 = new int[2][5];
     public static double[][] arr2 = new double[2][5];
     public static String[][] arr3 = new String[2][5];
-    static String chars = "abcdefghijklmnopqrstuvwxyz0987654321";
+    static String chars = "abcdefghijklmnopqrstuvwxyz";
 
     Scanner scanner = new Scanner(System.in);
     StringBuilder SERVER_PORT = new StringBuilder();
@@ -24,23 +27,45 @@ public class Server {
         }
         for (int i = 0; i < arr2.length; i ++) {
             for (int j = 0; j < arr2[i].length; j ++) {
-                arr2[i][j] = Math.random()*100;
+                arr2[i][j] = Math.random() * 100;
             }
         }
         Random random = new Random();
         for (int i = 0; i < arr3.length; i++) {
             for (int j = 0; j < arr3[i].length; j++) {
-                arr3[i][j] = String.valueOf((chars.charAt(random.nextInt(chars.length()))));
+                arr3[i][j] = String.valueOf(chars.charAt(random.nextInt(chars.length())));
             }
         }
     }
 
-    public static void changeCell(int row, int column, int elem) {
-        arr1[row][column] = elem;
-        System.out.println(Arrays.deepToString(arr1));
+    public static String changeStrArr(int row, int column, String elem) {
+        arr3[row][column] = elem;
+        return Arrays.deepToString(arr3);
     }
 
-    public Server() {
+    public static String changeDoubleArr(int row, int column, double elem) {
+        arr2[row][column] = elem;
+        return Arrays.deepToString(arr2);
+    }
+
+    public static String changeIntArr(int row, int column, int elem) {
+        arr1[row][column] = elem;
+        return Arrays.deepToString(arr1);
+    }
+
+    public Server(String[] args) {
+
+        List<Integer> restricted = new ArrayList<>();
+
+        for (String arg : args) {
+            int elem = Integer.parseInt(arg);
+            restricted.add(elem);
+        }
+
+        int restrictRow = restricted.get(0);
+        int restrictCol = restricted.get(1);
+
+        System.out.println("Запрещенный для изменения индекс массивов: " + restricted);
 
         System.out.println("Введите путь до файла, в котором хранится значение порта: ");
         String portFile = scanner.nextLine();
@@ -55,26 +80,71 @@ public class Server {
             }
 
             System.out.println(SERVER_PORT);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
         }
         try (ServerSocket server = new ServerSocket(Integer.parseInt(String.valueOf(SERVER_PORT)))) {
             System.out.println("Server started!");
             fillArray();
             Socket socket = server.accept();
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
             while (true) {
                 String response = "200-OK";
                 writer.println(response);
-                writer.flush();
                 String request = reader.readLine();
                 System.out.println("Request: " + request);
                 if (request.equals("int")){
-                    int row = Integer.parseInt(reader.readLine());
-                    int column = Integer.parseInt(reader.readLine());
-                    int elem = Integer.parseInt(reader.readLine());
-                    changeCell(row,column,elem);
+                    try {
+                        int row = Integer.parseInt(reader.readLine());
+                        int column = Integer.parseInt(reader.readLine());
+                        int elem = Integer.parseInt(reader.readLine());
+
+                        if (row == restrictRow && column == restrictCol) {
+                            writer.println("Индекс массива запрещен для изменений!");
+                        }
+                        else {
+                            writer.println(changeIntArr(row,column,elem));
+                        }
+                    } catch (Exception e) {
+                       writer.println(e.getMessage());
+                       writer.flush();
+                    }
+                }
+                else if (request.equals("double")){
+                    try {
+                        int row = Integer.parseInt(reader.readLine());
+                        int column = Integer.parseInt(reader.readLine());
+                        double elem = Double.parseDouble(reader.readLine());
+
+                        if (row == restrictRow && column == restrictCol) {
+                            writer.println("Индекс массива запрещен для изменений!");
+                        }
+                        else {
+                            writer.println(changeDoubleArr(row,column,elem));
+                        }
+                    } catch (Exception e) {
+                        writer.println(e.getMessage());
+                        writer.flush();
+                    }
+                }
+                else if (request.equals("str")){
+                    try {
+                        int row = Integer.parseInt(reader.readLine());
+                        int column = Integer.parseInt(reader.readLine());
+                        String elem = reader.readLine();
+
+                        if (row == restrictRow && column == restrictCol) {
+                            writer.println("Индекс массива запрещен для изменений!");
+                        }
+                        else {
+                            writer.println(changeStrArr(row, column, elem));
+                        }
+                    } catch (Exception e) {
+                        writer.println(e.getMessage());
+                        writer.flush();
+                    }
                 }
                 if (request.equals("close-socket")) {
                     reader.close();
@@ -84,8 +154,5 @@ public class Server {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-    public static void main(String[] args) {
-        new Server();
     }
 }
